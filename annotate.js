d54f91fff7490d609758c3d858d4f7507bd9b301
groqa <160558857+groqa@users.noreply.github.com>
2021-09-06 10:10:53 +0000

    fix shebang

--- annotate.js
+++ annotate.js
@@ -1,2 +1,6 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) |
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try
