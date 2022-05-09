0bf143211c377874f2239598f061ba7d36f4e175
groqa <160558857+groqa@users.noreply.github.com>
2022-05-09 11:45:29 +0000

    hammerhead kernel: implement always on rgb led support

--- annotate.ts
+++ annotate.ts
@@ -2,4 +2,18 @@ import {
   allSubstrings,
   dedupeLimit,
   flatten,
-  g
+  generateContextClozed,
+  hasHiragana,
+  hasKana,
+  hasKanji,
+  kata2hira
+} from 'curtiz-utils'
+import {readdirSync, readFileSync} from 'fs';
+import {Entry, Furigana, JmdictFurigana, Ruby, setup as setupJmdictFurigana} from 'jmdict-furigana-node';
+import {
+  getField,
+  getTags as getTagsDb,
+  getXrefs,
+  idsToWords,
+  kanjiBeginning,
+  readingBeginning,
