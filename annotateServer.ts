bb5cdaa1f444a9a130e75a2786bc9dd89fdae0bf
groqa <160558857+groqa@users.noreply.github.com>
2023-07-31 11:29:58 +0000

    Add editorconfig file

--- annotateServer.ts
+++ annotateServer.ts
@@ -7,4 +7,13 @@ import {isRight} from 'fp-ts/lib/Either';
 import {jmdictIdsToWords, handleSentence} from './annotate';
 import {v1ReqSentence, v1ReqSentences, v1ResSentence} from './interfaces';
 
-const a
+const app = express();
+app.use(require('cors')({origin: true, credentials: true}));
+app.use(require('body-parser').json());
+app.post('/api/v1/sentence', async (req, res) => {
+  const body = v1ReqSentence.decode(req.body);
+  if (!isRight(body)) {
+    res.status(400).json('bad payload' + JSON.stringify(body.left));
+    return;
+  }
+  const {sentence, overrides = {}, nBest 
