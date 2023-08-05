ac36d582372038b3061221479300269c001c3a00
groqa <160558857+groqa@users.noreply.github.com>
2023-08-05 08:37:39 +0000

    nitpicks

--- chino-particles.js
+++ chino-particles.js
@@ -3,4 +3,13 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-Object.definePro
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = require("fs");
+const path_1 = __importDefault(require("path"));
+const idxChinoParticles = fs_1.readFileSync(path_1.default.join(__dirname, 'chino-all-about-particles.txt'), 'utf8')
+    .trim()
+    .split('\n')
+    .map((s, i) => [i + 1, s.split('・')]);
+function lookup(raw) {
+    const ret = [];
+    if (raw.l
