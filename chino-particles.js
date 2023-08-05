08d7fdd9a58926816889c64900cd1bbeade47231
groqa <160558857+groqa@users.noreply.github.com>
2023-08-05 08:14:51 +0000

    minor style changes

---
+++ chino-particles.js
@@ -0,0 +1,6 @@
+"use strict";
+// Based on Naoko Chino's *All About Particles* (Kodansha)
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.definePro
