e09651049c826e723150144498e93dd06370b1e0
groqa <160558857+groqa@users.noreply.github.com>
2023-08-09 21:45:40 +0000

    libceph: have messages take a connection reference

--- kana.js
+++ kana.js
@@ -15,4 +15,9 @@ hiragana.split('').forEach((h, i) => {
 });
 function kata2hira(s) { return s.split('').map(c => exports.kata2hiraMap.get(c) || c).join(''); }
 exports.kata2hira = kata2hira;
-function hira2kata(s) { return s.split('').map(c => exports.hira
+function hira2kata(s) { return s.split('').map(c => exports.hira2kataMap.get(c) || c).join(''); }
+exports.hira2kata = hira2kata;
+/*
+There are other ways of doing this. In Unicode, katakana is 96 codepoints above hiragana. So
+`String.fromCharCode(hiragana.charCodeAt(0) + 96)` will produce katakana. In speed tests though, the above Map-based
+approach had the least variability in runtime (200 to 800 microseconds), while arithmetic-ba
