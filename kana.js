377c3eec7564dedb4b28cc9af1df1fad32982301
groqa <160558857+groqa@users.noreply.github.com>
2023-08-09 20:42:07 +0000

    CVS_SILENT Corrected typographical errors

--- kana.js
+++ kana.js
@@ -1,2 +1,10 @@
 "use strict";
-Object.defineProperty(exports, "__esMod
+Object.defineProperty(exports, "__esModule", { value: true });
+let hiragana = "ぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなに" +
+    "ぬねのはばぱひびぴふぶぷへべぺほぼまみむめもゃやゅゆょよらりるれろゎわゐゑをんゔゕゖ";
+let katakana = "ァアィイゥウェエォオカガキギクグケゲコゴサザシジスズセゼソゾタダチヂッツヅテデトドナニ" +
+    "ヌネノハバパヒビピフブプヘベペホボマミムメモャヤュユョヨラリルレロヮワヰヱヲンヴヵヶ";
+if (hiragana.length !== katakana.length) {
+    throw new Error('Kana strings not same length?');
+}
+exports.kata2hiraMap = new 
