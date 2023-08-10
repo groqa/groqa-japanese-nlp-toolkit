43ad48d4d92edd5745cb483736eab20b2631e94f
groqa <160558857+groqa@users.noreply.github.com>
2023-08-10 08:24:14 +0000

    Improved doc.

--- kana.js
+++ kana.js
@@ -20,4 +20,6 @@ exports.hira2kata = hira2kata;
 /*
 There are other ways of doing this. In Unicode, katakana is 96 codepoints above hiragana. So
 `String.fromCharCode(hiragana.charCodeAt(0) + 96)` will produce katakana. In speed tests though, the above Map-based
-approach had the least variability in runtime (200 to 800 microseconds), while arithmetic-ba
+approach had the least variability in runtime (200 to 800 microseconds), while arithmetic-based approaches used 100 to
+1500 microseconds.
+*/
