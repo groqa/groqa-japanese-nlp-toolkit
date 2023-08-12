2f92752de1b3a92b2e07650168dd9eed057ed87e
groqa <160558857+groqa@users.noreply.github.com>
2023-08-12 18:15:44 +0000

    Fixed broken links.

--- kanjidic.js
+++ kanjidic.js
@@ -14,4 +14,9 @@ const node_gzip_1 = require("node-gzip");
 const xml2js_1 = require("xml2js");
 const KANJIDIC_FILE = 'kanjidic2.xml.gz';
 function setup() {
-  
+    return __awaiter(this, void 0, void 0, function* () {
+        if (!fs_1.existsSync(KANJIDIC_FILE)) {
+            console.error(`Kanjidic2 missing. Download ${KANJIDIC_FILE} from http://www.edrdg.org/wiki/index.php/KANJIDIC_Project.`);
+            process.exit(1);
+        }
+        const raw = (yield node_gzip_1.ungzip(fs_1.readFileSync(KANJIDIC_FILE)
