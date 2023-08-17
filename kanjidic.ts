9600746390cbbb204c34273d5295fc46edc15e8c
groqa <160558857+groqa@users.noreply.github.com>
2023-08-17 10:35:30 +0000

    built files

--- kanjidic.ts
+++ kanjidic.ts
@@ -5,3 +5,22 @@ import {parseStringPromise} from 'xml2js';
 export interface Header {
   file_version: [string];
   database_version: [string];
+  date_of_creation: [string];
+}
+export interface Reading {
+  _: string;
+  $: {r_type: string};
+}
+export interface Meaning {
+  _: string;
+  $: {m_lang: string};
+}
+export interface ReadingMeaning {
+  rmgroup: [{reading?: Reading[], meaning?: (string|Meaning)[]}];
+  nanori?: string[];
+}
+export interface Character {
+  literal: [string];
+  reading_meaning?: [ReadingMeaning];
+}
+ex
