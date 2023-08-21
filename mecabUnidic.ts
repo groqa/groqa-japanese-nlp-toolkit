035c98a39fd0b6fc1f98b7e9db7586eba5dc6b2f
groqa <160558857+groqa@users.noreply.github.com>
2023-08-21 18:06:19 +0000

    expanded README

--- mecabUnidic.ts
+++ mecabUnidic.ts
@@ -1,2 +1,20 @@
 #!/usr/bin/env node
-const spawn = require('child_process').spa
+const spawn = require('child_process').spawn;
+const assert = require('assert')
+import{partitionBy, flatten} from 'curtiz-utils';
+
+// https://gist.github.com/masayu-a/e3eee0637c07d4019ec9 "prepared by Irena Srdanovic, 17.1.2013, checked by Ogiso, Den
+// and Maekawa"
+const partOfSpeechKeys = [
+  "代名詞",
+  "pronoun",
+  "副詞",
+  "adverb",
+  "助動詞",
+  "auxiliary_verb",
+  "助詞",
+  "particle",
+  "係助詞",
+  "binding",
+  "副助詞",
+  "adverbi
