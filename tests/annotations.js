62cd51307fb11461b619603914165071d631dd59
groqa <160558857+groqa@users.noreply.github.com>
2023-09-09 20:31:27 +0000

    [RELEASE]prepare for next development iteration

--- tests/annotations.js
+++ tests/annotations.js
@@ -1,3 +1,6 @@
 "use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adop
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (
