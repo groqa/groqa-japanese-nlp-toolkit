c496a35668582481d580873baaedc7939eaafff4
groqa <160558857+groqa@users.noreply.github.com>
2023-09-16 06:36:35 +0000

    Sync yarn.lock with master

--- tests/annotations.ts
+++ tests/annotations.ts
@@ -9,4 +9,14 @@ test('chatta', async t => {
   // in this sentence, Jdepp makes ことちゃった a bunsetsu
   const sentence = 'それは昨日のことちゃった';
   const x = (await annotate.handleSentence(sentence))[0];
-  if (typeof x === 'string' || 
+  if (typeof x === 'string' || !x.clozes) { throw new Error('assert') }
+  const conj = x.clozes?.conjugatedPhrases;
+  const deconj = conj.map(o => o.deconj);
+  t.ok(deconj.length);
+  t.ok(deconj.some(v => (v as Ugh<typeof v>).some(o => o.result.includes('ちゃった'))));
+  t.end();
+});
+test('denwa suru', async t => {
+  // in this sentence, Jdepp makes 電話 し ます a bunsetsu
+  const sentence = '彼に電話します';
+  co
